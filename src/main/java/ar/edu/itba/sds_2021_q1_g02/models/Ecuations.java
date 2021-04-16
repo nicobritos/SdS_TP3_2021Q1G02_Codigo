6fc50b6aa15f820f78a6725790b0e7caa9773af5
luciatorrusio <lucia.torrusio@gmail.com>
2021-04-16 10:34:11 -0300

    collision with particles equation implemented

--- src/main/java/ar/edu/itba/sds_2021_q1_g02/models/Ecuations.java
+++ src/main/java/ar/edu/itba/sds_2021_q1_g02/models/Ecuations.java
@@ -1,22 +1,18 @@
 package ar.edu.itba.sds_2021_q1_g02.models;
 
+import java.util.Collection;
+
 public class Ecuations {
     public boolean NoOverlap(Particle p1){
         for(Particle p2 : p1.getNeighbors()){
-            if(xyOverlap(p1,p2) < radiusOverlap(p1,p2))){
+            if(xyOverlap(p1,p2) < radiusOverlap(p1,p2)){
                 return false;
             }
         }
         return true;
     }
-    private double xyOverlap(Particle p1, Particle p2){
-        return Math.pow(p1.getPosition().x - p2.getPosition().x, 2) +Math.pow(p1.getPosition().y - p2.getPosition().y, 2)
-    }
-    private double radiusOverlap(Particle p1, Particle p2){
-        return Math.pow(p1.getRadius() + p2.getRadius(), 2) ;
-    }
     // returns the min time that it will collide with a wall
-    public double CollitionWall(Particle p){
+    public double CollisionWall(Particle p){
         double x0 = p.getPosition().getX();
         double y0 = p.getPosition().getY();
         double xWall, yWall, tc, tcAux, tcAux1;
@@ -60,10 +56,70 @@ public class Ecuations {
         return tc;
     }
 
+    public double CollisionParticles(Particle p, Collection<Particle> particles){
+        double tc,d, dTimesR, tcAux;
+        tc = Double.POSITIVE_INFINITY;
+        boolean firstParticle = true;
+        for (Particle p2 : particles){
+            d = d(p,p2);
+            dTimesR =deltaVTimesdeltaR(p,p2);
+            if(d(p,p2)> 0 && dTimesR<0 && !p2.equals(p)){
+                tcAux = (- dTimesR + Math.sqrt(d))/deltaVTimesdeltaV(p,p2);
+                if(firstParticle || tcAux < tc ){
+                    firstParticle = false;
+                    tc = tcAux;
+                }
+            }
+        }
+        return tc;
+    }
+
+
+    private Velocity deltaV(Particle p1, Particle p2){
+        return  new Velocity(deltaVx(p1,p2), deltaVy(p1,p2));
+    }
+    private Position deltaR(Particle p1, Particle p2){
+        return  new Position(deltaX(p1,p2), deltaY(p1,p2));
+    }
+    private double tita(Particle p1, Particle p2){
+        return p1.getRadius() + p2.getRadius();
+    }
+    private double deltaVTimesdeltaV(Particle p1, Particle p2){
+        return xyOverlap(p1,p2);
+    }
+    private double deltaVTimesdeltaR(Particle p1, Particle p2){
+        return deltaVx(p1,p2)*deltaX(p1,p2)+ deltaVy(p1,p2)*deltaY(p1,p2);
+    }
+    private double deltaVx(Particle p1, Particle p2){
+        return p2.getVelocity().getxSpeed() - p1.getVelocity().getxSpeed();
+    }
+    private double deltaVy(Particle p1, Particle p2){
+        return p2.getVelocity().getySpeed() - p1.getVelocity().getySpeed();
+    }
+    private double deltaX(Particle p1, Particle p2){
+        return p2.getPosition().getX() - p1.getPosition().getX();
+    }
+    private double deltaY(Particle p1, Particle p2){
+        return p2.getPosition().getY() - p1.getPosition().getY();
+    }
+    private double d(Particle p1, Particle p2){
+        return Math.pow(deltaVTimesdeltaR(p1,p2), 2)-deltaVTimesdeltaV(p1,p2)*(deltaRTimesdeltaR(p1,p2) - Math.pow(tita(p1,p2), 2));
+    }
+    private double deltaRTimesdeltaR(Particle p1, Particle p2){
+        return Math.pow(deltaX(p1,p2),2)+ Math.pow(deltaY(p1,p2),2);
+    }
+    private double xyOverlap(Particle p1, Particle p2){
+        return Math.pow(deltaX(p1,p2), 2) +Math.pow(deltaY(p1,p2), 2);
+    }
+    private double radiusOverlap(Particle p1, Particle p2){
+        return Math.pow(p1.getRadius() + p2.getRadius(), 2) ;
+    }
     private Position getParticlePos(double time, Particle p){
         Position newPos = new Position(0,0);
         newPos.setX(p.getPosition().getX() + time * p.getVelocity().getxSpeed());
         newPos.setY(p.getPosition().getY() + time * p.getVelocity().getySpeed());
         return newPos;
     }
+
+
 }
