55369f8930a692edd27f4e832cffc798d86be2c6
luciatorrusio <lucia.torrusio@gmail.com>
2021-04-16 11:32:35 -0300

    added evolve particles

--- src/main/java/ar/edu/itba/sds_2021_q1_g02/models/Equations.java
+++ src/main/java/ar/edu/itba/sds_2021_q1_g02/models/Equations.java
@@ -1,8 +1,8 @@
 package ar.edu.itba.sds_2021_q1_g02.models;
 
-import java.util.Collection;
+import java.util.*;
 
-public class Ecuations {
+public class Equations {
     public boolean NoOverlap(Particle p1){
         for(Particle p2 : p1.getNeighbors()){
             if(xyOverlap(p1,p2) < radiusOverlap(p1,p2)){
@@ -17,7 +17,7 @@ public class Ecuations {
         double y0 = p.getPosition().getY();
         double xWall, yWall, tc, tcAux, tcAux1;
 
-        // collition with vertical walls
+        // collision with vertical walls
         if(p.getVelocity().getxSpeed() > 0){
             xWall = 0.24;
             tc = (xWall - p.getRadius() - x0)/p.getVelocity().getxSpeed();
@@ -26,7 +26,7 @@ public class Ecuations {
             xWall = 0;
             tc = (xWall + p.getRadius() - x0)/p.getVelocity().getxSpeed();
         }
-        // collition with horizontal walls
+        // collision with horizontal walls
         if(p.getVelocity().getySpeed() > 0){
             yWall = 0.9;
             tcAux = (yWall - p.getRadius() - y0)/p.getVelocity().getySpeed();
@@ -37,7 +37,7 @@ public class Ecuations {
         }
         if(tc>tcAux){ tc = tcAux;}
 
-        // collition with intermediate walls (vertical)
+        // collision with intermediate walls (vertical)
         if(p.getVelocity().getxSpeed() > 0 && p.getPosition().getX() < 0.12){
             xWall = 0.12;
             tcAux1 = (xWall - p.getRadius() - x0)/p.getVelocity().getxSpeed();
@@ -74,7 +74,38 @@ public class Ecuations {
         return tc;
     }
 
+    public Collection<Particle> EvolveParticlesPositions(Collection<Particle> particles, double tc){
+        for(Particle p : particles){
+            p.getPosition().setX(p.getPosition().getX() + p.getVelocity().getxSpeed() * tc);
+            p.getPosition().setY(p.getPosition().getY() + p.getVelocity().getySpeed() * tc);
+        }
+        return particles;
+    }
 
+    public Velocity EvolveParticleVelocities(Particle p1, boolean vertical){
+        if(vertical){
+            return new Velocity(-p1.getVelocity().getxSpeed(), p1.getVelocity().getySpeed());
+        }
+        return new Velocity(p1.getVelocity().getxSpeed(), -p1.getVelocity().getySpeed());
+    }
+    public List<Velocity> EvolveParticleVelocities(Particle p1, Particle p2){
+        Velocity v1d = new Velocity(p1.getVelocity().getxSpeed() +(Jx(p1,p2)/p1.getMass()), p1.getVelocity().getySpeed() +(Jy(p1,p2)/p1.getMass()));
+        Velocity v2d = new Velocity(p2.getVelocity().getxSpeed() -(Jx(p1,p2)/p2.getMass()), p2.getVelocity().getySpeed() -(Jy(p1,p2)/p2.getMass()));
+        List<Velocity> v = new ArrayList<Velocity>();
+        v.add(v1d);
+        v.add(v2d);
+        return v;
+    }
+
+    private double J(Particle p1, Particle p2 ){
+        return (2*p1.getMass()*p2.getMass()*deltaVTimesdeltaR(p1,p2))/(tita(p1,p2)*(p1.getMass()+p2.getMass()));
+    }
+    private double Jx(Particle p1, Particle p2){
+        return J(p1,p2)*deltaX(p1,p2)/tita(p1,p2);
+    }
+    private double Jy(Particle p1, Particle p2){
+        return J(p1,p2)*deltaY(p1,p2)/tita(p1,p2);
+    }
     private Velocity deltaV(Particle p1, Particle p2){
         return  new Velocity(deltaVx(p1,p2), deltaVy(p1,p2));
     }
